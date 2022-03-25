8ddaa5d4c6e6bbd9a8891412d5bbf8ac2078fc0f
Faik Burak Türedi <burak@thestarware.com>
2022-03-26 02:50:33 +0300

    complated tailwindcss configuration
    and also added firebase libarary

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-
-
 import './App.css'
 
 export default function App() {
     return (
         <div>
-            <h1>SchedulEasy</h1>
+            <h1>SchedulEasy</h1> 
         </div>
     );
 }
