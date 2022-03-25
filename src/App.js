4632ceae4fd5b80a564f94b5dbcead733e338644
Faik Burak Türedi <burak@thestarware.com>
2022-03-26 00:09:30 +0300

    fixed router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-import LoginPage from './components/LoginPage'
-import RegisterPage from './components/RegisterPage'
-import ForgetPasswordPage from './components/ForgetPasswordPage'
 
 import './App.css'
 
 export default function App() {
     return (
-        <Router>
-            <div>
-                <Switch>
-                    <Route path="/login" component={ LoginPage } />
-                    <Route path="/register" component={ RegisterPage } />
-                    <Route path="/forget-password" component={ ForgetPasswordPage } />
-                </Switch>
-            </div>
-        </Router>
-    )
+        <div>
+            <h1>SchedulEasy</h1>
+        </div>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route,Routes } from 'react-router-dom'
+import App from "./App";
+import LoginPage from './components/LoginPage'
+import RegisterPage from './components/RegisterPage'
+import ForgetPasswordPage from './components/ForgetPasswordPage'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={ <LoginPage/> } />
+        <Route path="/register" element={ <RegisterPage/> } />
+        <Route path="/forget-password" element={ <ForgetPasswordPage/> } />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
