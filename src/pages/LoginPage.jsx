8d3f3d279cc44f6e5fc8ba0592a0823037f65648
gokturkturan <gktrktrn673@gmail.com>
2022-03-28 22:52:57 +0300

    Added CSS to Login Page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,53 +1,108 @@
-import React, { Fragment } from 'react'
+import React from "react";
 import { useState } from "react";
-
-import { Link } from 'react-router-dom'
-
-import '../App.css'
-import Navbar from '../components/Navbar';
+import { Link } from "react-router-dom";
+import "../App.css";
+import { LockClosedIcon } from "@heroicons/react/solid";
 
 export default function SignInPage() {
+  const [inputs, setInputs] = useState({});
 
-    const [inputs, setInputs] = useState({});
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  };
 
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({...values, [name]: value}))
-      }
+  return (
+    <>
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            {/*
+            <img
+            
+            />
+            */}
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+          <form
+            className="mt-8 space-y-6"
+            action="/home"
+            onSubmit={handleSubmit}
+          >
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label className="sr-only">Email address</label>
+                <input
+                  type="text"
+                  name="email"
+                  value={inputs.email}
+                  onChange={handleChange}
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label className="sr-only">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={inputs.password || ""}
+                  onChange={handleChange}
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
 
-      const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(inputs);
-      }
+            <div className="flex items-center justify-between">
+              <div className="flex items-center"></div>
+              <div className="text-sm">
+                <Link to="/forget-password">
+                  <label className="font-medium text-blue-700 hover:text-blue-500">
+                    Forgot password?
+                  </label>
+                </Link>
+              </div>
+            </div>
 
-    return (
-        <Fragment>
-        <Navbar/>
-        <div className="text-center m-5-auto">
-            <h2>Sign in</h2>
-            <form action="/home" onSubmit={handleSubmit}>
-                <p>
-                    <label>Email</label><br/>
-                    <input type="text" name="email" value={inputs.email} 
-                            onChange={handleChange} required />
-                </p>
-                <p>
-                    <label>Password</label>
-                    <Link to="/forget-password"><label className="right-label">Forget password?</label></Link>
-                    <br/>
-                    <input type="password" name="password" value={inputs.password || ""} 
-                          onChange={handleChange} required />
-                </p><br/>
-                <p>
-                    <button id="sub_btn" type="submit">Login</button>
-                </p>
-            </form>
-            <footer>
-                <p> <Link to="/register">Create an account</Link>.</p>
-            </footer>
+            <div>
+              <button
+                id="sub_btn"
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-700 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-blue-500 group-hover:text-blue-700"
+                    aria-hidden="true"
+                  />
+                </span>
+                Sign in
+              </button>
+              <br />
+              <div className="text-center text-1xl  text-gray-900">
+                Don't have an account yet? &nbsp;
+                <Link
+                  to="/register"
+                  className="font-medium text-blue-700 hover:text-blue-500"
+                >
+                  Sign up
+                </Link>
+              </div>
+            </div>
+          </form>
         </div>
-        </Fragment>
-    )
+      </div>
+    </>
+  );
 }
