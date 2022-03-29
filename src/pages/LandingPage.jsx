4637a8da6c7b67afa263edbfcb12ca7e083a3099
Faik Burak Türedi <fburakturedi@hotmail.com>
2022-03-29 04:43:55 +0300

    added google and email auth into project
    also added greetings message.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "../components/Navbar";
+import AuthContext from "../store/authContext";
+import { Notification } from "../UI/Notification";
+
 
 const LandingPage = () => {
-  return <Navbar />;
+  const authCtx = useContext(AuthContext);
+  const userInfo =JSON.parse( localStorage.getItem("userInfo"));
+  return( 
+  <>
+  <Navbar />
+  {authCtx.firstLoginG && <Notification status="Succ" title={`Welcome ${userInfo.userName} !`} message="Deneme" disappear={true}/>}
+  </>);
 };
 
 export default LandingPage;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,13 +1,19 @@
-import React from "react";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import React,{useState,useContext} from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "../App.css";
 import { LockClosedIcon } from "@heroicons/react/solid";
 import logo from "../components/Logo.png";
 import Navbar from "../components/Navbar";
+import { signInWithPopup, signInWithEmailAndPassword, GoogleAuthProvider } from "firebase/auth";
+import { auth, provider } from "../database/firebase-config";
+import AuthContext from "../store/authContext";
+import { DatabaseHandler } from "../database/DatabaseHandler";
+
 
 export default function SignInPage() {
   const [inputs, setInputs] = useState({});
+  const authCtx = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const handleChange = (event) => {
     const name = event.target.name;
@@ -18,15 +24,58 @@ export default function SignInPage() {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(inputs);
+    signInWithEmailAndPassword(auth, inputs.email, inputs.password)
+    .then(async (userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+    const userName = await DatabaseHandler.getUserName(user.uid); // Critical ! I should look again. undefined
+    console.log(userName);
+    localStorage.setItem("uid",user.uid);
+    localStorage.setItem("userInfo",JSON.stringify({email:user.email,userName:userName}))
+    authCtx.onLogin();
+    navigate("/");
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode,errorMessage);
+  });
   };
+
+  const googleHandler = ()=>{
+
+    signInWithPopup(auth,provider).then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      console.log(user,user.uid);
+      localStorage.setItem("uid",user.uid);
+      localStorage.setItem("userInfo",JSON.stringify({
+        email: user.email,
+        userName: user.displayName
+      }))
+      authCtx.onLogin();
+      navigate("/");
+      
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      console.log(errorCode,errorMessage,email,credential);
+
+    });
+  }
+
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <img className="mx-auto h-50 w-72" src={logo} alt="logo"/>
+            <img className="mx-auto h-50 w-72" src={logo} alt="logo" />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
               Sign in to your account
             </h2>
@@ -77,16 +126,30 @@ export default function SignInPage() {
             <div>
               <button
                 id="sub_btn"
+                className="group relative w-full flex justify-center py-3 px-4 border border-transparent text-base font-medium rounded-md text-white bg-blue-700 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-700 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                <span className="absolute left-0 inset-y-0 flex items-center pl-5">
                   <LockClosedIcon
                     className="h-5 w-5 text-blue-500 group-hover:text-blue-700"
                     aria-hidden="true"
                   />
                 </span>
                 Sign in
+              </button>
+              <br />
+              <button
+                className="group relative w-full flex justify-center py-3 px-4 border border text-sm font-medium rounded-md text-black bg-white hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                type="button"
+                onClick={googleHandler}
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-5">
+                <img
+                  src="https://img.icons8.com/ios-filled/30/000000/google-logo.png"
+                  alt="google icon"
+                />
+                </span>
+                Continue with Google
               </button>
               <br />
               <div className="text-center text-1xl  text-gray-900">
