cdb1d6119a3c408281cd32eaa696f897c2c1bf5d
Faik Burak Türedi <fburakturedi@hotmail.com>
2022-03-29 04:42:49 +0300

    added conditional rendering into Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { Link } from "react-router-dom";
+import AuthContext from "../store/authContext";
 import "./Navbar.css";
 
-function Navbar() {
+
+
+
+
+const Navbar = () => {
+  const authCtx= useContext(AuthContext);
+  
+  const unAuthorized = [
+    <Link to="/register">
+      <button className="inline-flex items-center px-3 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-blue-900 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        Sign Up
+      </button>
+      &nbsp;
+      <Link to="/login">
+        <button className="inline-flex items-center px-3 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-blue-900 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Login
+        </button>
+      </Link>
+    </Link>];
+
+  const authorized = [
+    <button onClick={()=>{
+      authCtx.onLogout();
+      
+    }} className="inline-flex items-center px-3 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-blue-900 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+    Logout
+    </button>];
   return (
     <>
       <nav className="navbar ">
@@ -32,17 +59,8 @@ function Navbar() {
             </Link>
           </li>
           <div className="my-2.5">
-            <Link to="/register">
-              <button className="inline-flex items-center px-3 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-blue-900 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Sign Up
-              </button>
-              &nbsp;
-              <Link to="/login">
-                <button className="inline-flex items-center px-3 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-blue-900 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                  Login
-                </button>
-              </Link>
-            </Link>
+          {!authCtx.isLoggedIn && unAuthorized}
+          {authCtx.isLoggedIn && authorized}
           </div>
         </ul>
       </nav>
