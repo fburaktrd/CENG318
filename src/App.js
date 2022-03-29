c08f7c00e68fcea13ab807005ca74223bd663960
Faik Burak Türedi <fburakturedi@hotmail.com>
2022-03-29 04:41:06 +0300

    updated index.js and App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ForgetPasswordPage from "./pages/ForgetPasswordPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    <div>
-      <LandingPage />
-    </div>
+
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/forget-password" element={<ForgetPasswordPage />} />
+    </Routes>
+  </BrowserRouter>
+    
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import App from "./App";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import ForgetPasswordPage from "./pages/ForgetPasswordPage";
+import { AuthContextProvider } from "./store/authContext";
+
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/forget-password" element={<ForgetPasswordPage />} />
-    </Routes>
-  </BrowserRouter>,
+  <AuthContextProvider><App/></AuthContextProvider>,
   rootElement
 );
