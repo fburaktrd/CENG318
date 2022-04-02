3ef80fbe1b3619634e51367c3531f8efa17e6516
gokturkturan <gktrktrn673@gmail.com>
2022-04-02 20:36:47 +0300

    Redesign Navbar and Added TeamPage

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ForgetPasswordPage from "./pages/ForgetPasswordPage";
+import TeamPage from "./pages/TeamPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/forget-password" element={<ForgetPasswordPage />} />
-    </Routes>
-  </BrowserRouter>
-    
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/loginPage" element={<LoginPage />} />
+        <Route path="/registerPage" element={<RegisterPage />} />
+        <Route path="/forgetPasswordPage" element={<ForgetPasswordPage />} />
+        <Route path="/teamPage" element={<TeamPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,83 @@
 import React, { useContext } from "react";
-
+import NavLogo from "../components/NavLogo.png";
 import { Link } from "react-router-dom";
 import AuthContext from "../store/authContext";
-import "./Navbar.css";
-
-
-
-
+import { Disclosure } from "@headlessui/react";
 
 const Navbar = () => {
-  const authCtx= useContext(AuthContext);
-  
+  const authCtx = useContext(AuthContext);
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
+  const navigation = [
+    { name: "Home", to: "/", current: true },
+    { name: "Team", to: "/teamPage", current: false },
+    { name: "???", to: "/", current: false },
+    { name: "???", to: "/", current: false },
+  ];
+
   const unAuthorized = [
-    <Link to="/register">
-      <button className="inline-flex items-center px-3 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-blue-900 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-        Sign Up
-      </button>
+    <div>
+      <Link to="/registerPage">
+        <button className="inline-flex items-center px-3 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-blue-700 rounded-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          Sign Up
+        </button>
+      </Link>
       &nbsp;
-      <Link to="/login">
-        <button className="inline-flex items-center px-3 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-blue-900 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+      <Link to="/loginPage">
+        <button className="inline-flex items-center px-3 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-blue-700 rounded-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Login
         </button>
       </Link>
-    </Link>];
+    </div>,
+  ];
 
   const authorized = [
-    <button onClick={()=>{
-      authCtx.onLogout();
-      
-    }} className="inline-flex items-center px-3 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-blue-900 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-    Logout
-    </button>];
+    <button
+      onClick={() => {
+        authCtx.onLogout();
+      }}
+      className="inline-flex items-center px-3 py-2 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-blue-900 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    >
+      Logout
+    </button>,
+  ];
   return (
-    <>
-      <nav className="navbar ">
-        <div to="/" className="navbar-logo">
-          LOGO KOYULACAK
-        </div>
-        <ul className="nav-menu">
-          <li className="nav-item">
-            <Link to="/" className="nav-links">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-links">
-              ???
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-links">
-              ???
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/" className="nav-links">
-              Contact
-            </Link>
-          </li>
-          <div className="my-2.5">
-          {!authCtx.isLoggedIn && unAuthorized}
-          {authCtx.isLoggedIn && authorized}
+    <Disclosure as="nav" className="bg-white-800 border-b-2">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center">
+              <img
+                className="hidden lg:block h-8 w-auto"
+                src={NavLogo}
+                alt="SchedulEasy"
+              />
+            </div>
+            <div className="hidden sm:block sm:ml-8">
+              <div className="flex space-x-4">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    to={item.to}
+                    className="text-black-300 hover:bg-blue-700 hover:text-white px-3 py-2 rounded-md text-base font-medium"
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
           </div>
-        </ul>
-      </nav>
-    </>
+          <div className="my-2.5">
+            {!authCtx.isLoggedIn && unAuthorized}
+            {authCtx.isLoggedIn && authorized}
+          </div>
+        </div>
+      </div>
+    </Disclosure>
   );
-}
+};
 
 export default Navbar;
