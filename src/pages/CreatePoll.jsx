68a8f7a09d6c57236dba46557230abd13f2d5ad6
Faik Burak Türedi <fburakturedi@hotmail.com>
2022-04-09 16:52:48 +0300

    uninstalled old libs

--- src/pages/CreatePoll.jsx
+++ src/pages/CreatePoll.jsx
@@ -1,31 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import { useNavigate } from "react-router-dom";
-import Calendar from "react-awesome-calendar";
 
-const events = [
-  {
-    id: 1,
-    color: "#fd3153",
-    from: "2019-05-02T18:00:00+00:00",
-    to: "2019-05-05T19:00:00+00:00",
-    title: "This is an event",
-  },
-  {
-    id: 2,
-    color: "#1ccb9e",
-    from: "2019-05-01T13:00:00+00:00",
-    to: "2019-05-05T14:00:00+00:00",
-    title: "This is another event",
-  },
-  {
-    id: 3,
-    color: "#3694DF",
-    from: "2019-05-05T13:00:00+00:00",
-    to: "2019-05-05T20:00:00+00:00",
-    title: "This is also another event",
-  },
-];
 
 const CreatePoll = () => {
   const navigate = useNavigate();
@@ -203,7 +179,6 @@ const CreatePoll = () => {
 
         <div className="pt-8">
           <center>Tarih/saat seçim alanı</center>
-          <Calendar events={events} />
         </div>
 
         <div className="pt-8">
