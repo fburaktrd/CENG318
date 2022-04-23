8b7827708777180dd71e3bc572955f9c5e0f0448
gokturkturan <gktrktrn673@gmail.com>
2022-04-23 18:53:50 +0300

    Landing part of HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -94,7 +94,53 @@ const HomePage = (props) => {
           </div>
         </div>
       ) : (
-        <div> you are not logged</div>
+        <main>
+          <div className="relative mt-16">
+            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+              <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden">
+                <div className="absolute inset-0">
+                  <img
+                    className="h-full w-full object-cover"
+                    src="https://www.trumpalisverismerkezi.com/blog/images/blog/evde-oynanan-oyunlar-640x427-min.jpg"
+                    alt="People working on laptops"
+                  />
+                  <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-blue-300 mix-blend-multiply" />
+                </div>
+                <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
+                  <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
+                    <span className="block text-white">
+                      Scheduling a Meeting
+                    </span>
+                    <span className="block text-gray-300">now much easier</span>
+                  </h1>
+                  <p className="mt-6 max-w-lg mx-auto text-center text-xl text-gray-300 sm:max-w-3xl">
+                    By planning your meetings on our site, you will be able to
+                    make quicker and faster decisions and avoid wasting time.
+                  </p>
+                  <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
+                    <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
+                      <Link
+                        to="/loginPage"
+                        className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-blue-700 bg-white hover:bg-indigo-50 sm:px-8"
+                      >
+                        Sign Up
+                      </Link>
+                      <Link
+                        to="/registerPage"
+                        className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-700 bg-opacity-60 hover:bg-opacity-70 sm:px-8"
+                      >
+                        Login
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p className="flex justify-center mt-8 text-base text-gray-400 md:mt-18 md:order-1">
+              &copy; 2022 SchedulEasy, Inc. All rights reserved.
+            </p>
+          </div>
+        </main>
       )}
     </>
   );
