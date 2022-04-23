f9903cd685702977dad906bf7b28edf03fc848d6
Faik Burak Türedi <fburakturedi@hotmail.com>
2022-04-23 19:38:47 +0300

    added ParticipantList comp

---
+++ src/components/ParticipantList.jsx
@@ -0,0 +1,19 @@
+
+
+export default function ParticipantList() {
+    return (
+        <>
+        <h5>Gelenler</h5>
+        <ul>
+
+            <li>Ahmet</li>
+            <li>Mehmet</li>
+            <li>Mehmet</li>
+            <li>Mehmet</li>
+            
+        </ul>
+        </>
+
+    
+    )
+  }
