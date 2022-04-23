516627439e23fe98ce9d1ee95e382053113d37e8
Faik Burak Türedi <fburakturedi@hotmail.com>
2022-04-23 19:06:24 +0300

    fixed bug

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -120,13 +120,13 @@ const HomePage = (props) => {
                   <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
                     <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
                       <Link
-                        to="/loginPage"
+                        to="/registerPage"
                         className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-blue-700 bg-white hover:bg-indigo-50 sm:px-8"
                       >
                         Sign Up
                       </Link>
                       <Link
-                        to="/registerPage"
+                        to="/loginPage"
                         className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-700 bg-opacity-60 hover:bg-opacity-70 sm:px-8"
                       >
                         Login
