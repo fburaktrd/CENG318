c5fffe9b5837673723d0edff2e98613f72ac1905
gokturkturan <gktrktrn673@gmail.com>
2022-04-23 13:49:33 +0300

    icons added

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -109,7 +109,21 @@ export default function SignInPage(props) {
             onSubmit={handleSubmit}
           >
             <div className="rounded-md shadow-sm -space-y-px">
-              <div>
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 15"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                  />
+                </svg>
                 <label className="sr-only">Email address</label>
                 <input
                   type="text"
@@ -121,7 +135,21 @@ export default function SignInPage(props) {
                   placeholder="Email address"
                 />
               </div>
-              <div>
+              <div className="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 18"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z"
+                  />
+                </svg>
                 <label className="sr-only">Password</label>
                 <input
                   type="password"
