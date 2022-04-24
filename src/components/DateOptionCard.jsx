2e14bcfd481a4c32b1308317982580d837603271
Faik Burak Türedi <fburakturedi@hotmail.com>
2022-04-24 22:38:16 +0300

    updated DateOptionCard

--- src/components/DateOptionCard.jsx
+++ src/components/DateOptionCard.jsx
@@ -1,3 +1,6 @@
+import { CalendarIcon, ClockIcon } from "@heroicons/react/outline";
+import { ClockIcon as ClockIconSolid } from "@heroicons/react/solid";
+
 export default function DateOptionCard(props) {
   return (
     <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
@@ -5,13 +8,24 @@ export default function DateOptionCard(props) {
         <li key={index} className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
           <div className="w-full flex items-center justify-between p-6 space-x-6">
             <div className="flex-1 truncate">
-              <div className="flex items-center space-x-3">
-                <h3 className="text-gray-900 text-sm font-medium truncate">{option.date}</h3>
+              <div className="flex items-center space-x-1">
+              <CalendarIcon height={15} width={15}/>
+                <h3 className="text-gray-900 text-sm font-medium truncate">
+                
+                  Date: {option.date}
+                </h3>
                 {/* <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
                   {person.role}
                 </span> */}
               </div>
-              <p className="mt-1 text-gray-500 text-sm truncate">{`Start Time: ${option.startTime} - End Time: ${option.endTime}`}</p>
+              <div className="flex items-center space-x-1">
+              <ClockIcon height={15} width={15}/>
+              <p className="mt-1 text-gray-500 text-sm md:text-clip flex-1">{`Start Time: ${option.startTime}`}</p>
+              </div>
+              <div className="flex items-center space-x-1">
+                <ClockIconSolid height={15} width={15}/>
+              <p className="mt-1 text-gray-500 text-sm md:text-clip flex-1">{`End Time: ${option.endTime}`}</p>
+              </div>
             </div>
           </div>
           <div>
