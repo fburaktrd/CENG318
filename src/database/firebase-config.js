a75d9f15119ea53ff1c39583bcc8e743e181643d
Faik Burak Türedi <fburakturedi@hotmail.com>
2022-04-25 11:24:38 +0300

    new db for presentation

--- src/database/firebase-config.js
+++ src/database/firebase-config.js
@@ -1,34 +1,56 @@
 // Import the functions you need from the SDKs you need
 
-import { initializeApp } from "firebase/app";
+import { initializeApp,getApps,getApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-
+//First one
 // Your web app's Firebase configuration
 
+// const firebaseConfig = {
+
+//   apiKey: "AIzaSyCiGtA9NHnFJ4TN1PxAbDgHl7UXfcjMoRM",
+
+//   authDomain: "scheduleasy-d17f5.firebaseapp.com",
+
+//   projectId: "scheduleasy-d17f5",
+
+//   storageBucket: "scheduleasy-d17f5.appspot.com",
+
+//   messagingSenderId: "645588070028",
+
+//   appId: "1:645588070028:web:b8d041823b7e552c9995a9"
+
+// };
+
+
+
+//Second One (for presentation)
 const firebaseConfig = {
 
-  apiKey: "AIzaSyCiGtA9NHnFJ4TN1PxAbDgHl7UXfcjMoRM",
+  apiKey: "AIzaSyCquiYBemUf1DPT4PAIwhnENQZAdvuCT6E",
 
-  authDomain: "scheduleasy-d17f5.firebaseapp.com",
+  authDomain: "scheduleasy2.firebaseapp.com",
 
-  projectId: "scheduleasy-d17f5",
+  databaseURL: "https://scheduleasy2-default-rtdb.firebaseio.com",
 
-  storageBucket: "scheduleasy-d17f5.appspot.com",
+  projectId: "scheduleasy2",
 
-  messagingSenderId: "645588070028",
+  storageBucket: "scheduleasy2.appspot.com",
 
-  appId: "1:645588070028:web:b8d041823b7e552c9995a9"
+  messagingSenderId: "331607045820",
+
+  appId: "1:331607045820:web:32d38e86e4a613aecd02b9"
 
 };
 
 
 // Initialize Firebase
 
-const app = initializeApp(firebaseConfig);
+//const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
