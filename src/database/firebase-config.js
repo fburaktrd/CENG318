2812f60030a620b415ff271f02a7515dff42549f
Faik Burak Türedi <fburakturedi@hotmail.com>
2022-06-03 11:30:30 +0300

    updated firebase

--- src/database/firebase-config.js
+++ src/database/firebase-config.js
@@ -7,44 +7,43 @@ import {getAuth,GoogleAuthProvider} from 'firebase/auth';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 //First one
-// Your web app's Firebase configuration
 
-// const firebaseConfig = {
+const firebaseConfig = {
 
-//   apiKey: "AIzaSyCiGtA9NHnFJ4TN1PxAbDgHl7UXfcjMoRM",
+  apiKey: "AIzaSyCiGtA9NHnFJ4TN1PxAbDgHl7UXfcjMoRM",
 
-//   authDomain: "scheduleasy-d17f5.firebaseapp.com",
+  authDomain: "scheduleasy-d17f5.firebaseapp.com",
 
-//   projectId: "scheduleasy-d17f5",
+  projectId: "scheduleasy-d17f5",
 
-//   storageBucket: "scheduleasy-d17f5.appspot.com",
+  storageBucket: "scheduleasy-d17f5.appspot.com",
 
-//   messagingSenderId: "645588070028",
+  messagingSenderId: "645588070028",
 
-//   appId: "1:645588070028:web:b8d041823b7e552c9995a9"
+  appId: "1:645588070028:web:b8d041823b7e552c9995a9"
 
-// };
+};
 
 
 
 //Second One (for presentation)
-const firebaseConfig = {
+// const firebaseConfig = {
 
-  apiKey: "AIzaSyCquiYBemUf1DPT4PAIwhnENQZAdvuCT6E",
+//   apiKey: "AIzaSyCquiYBemUf1DPT4PAIwhnENQZAdvuCT6E",
 
-  authDomain: "scheduleasy2.firebaseapp.com",
+//   authDomain: "scheduleasy2.firebaseapp.com",
 
-  databaseURL: "https://scheduleasy2-default-rtdb.firebaseio.com",
+//   databaseURL: "https://scheduleasy2-default-rtdb.firebaseio.com",
 
-  projectId: "scheduleasy2",
+//   projectId: "scheduleasy2",
 
-  storageBucket: "scheduleasy2.appspot.com",
+//   storageBucket: "scheduleasy2.appspot.com",
 
-  messagingSenderId: "331607045820",
+//   messagingSenderId: "331607045820",
 
-  appId: "1:331607045820:web:32d38e86e4a613aecd02b9"
+//   appId: "1:331607045820:web:32d38e86e4a613aecd02b9"
 
-};
+// };
 
 
 // Initialize Firebase
