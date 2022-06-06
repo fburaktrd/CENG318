5523b59e04618293026e768b3df801745e65f3c7
Faik Burak Türedi <fburakturedi@hotmail.com>
2022-06-06 12:26:24 +0300

    changed color

--- src/UI/RedirectingBanner.jsx
+++ src/UI/RedirectingBanner.jsx
@@ -2,11 +2,11 @@ import { SpeakerphoneIcon, XIcon,ArrowCircleRightIcon } from '@heroicons/react/o
 
 export default function RedirectinBanner() {
   return (
-    <div className="bg-indigo-600">
+    <div className="bg-blue-600">
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between flex-wrap">
           <div className="w-0 flex-1 flex items-center">
-            <span className="flex p-2 rounded-lg bg-indigo-800">
+            <span className="flex p-2 rounded-lg bg-blue-800">
               <ArrowCircleRightIcon className="h-6 w-6 text-white" aria-hidden="true" />
             </span>
             <p className="ml-3 font-medium text-white truncate">
@@ -20,7 +20,7 @@ export default function RedirectinBanner() {
           <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
             <button
               type="button"
-              className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+              className="-mr-1 flex p-2 rounded-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
             >
               <span className="sr-only">Close</span>
               <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
