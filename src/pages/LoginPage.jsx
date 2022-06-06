151be9439f7e3478e88840c208b80d2dc1939154
Faik Burak Türedi <fburakturedi@hotmail.com>
2022-06-06 15:36:56 +0300

    chaged popup instead redirecting for google auth

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -110,38 +110,38 @@ export default function SignInPage(props) {
   };
 
   const googleHandler = () => {
-    signInWithRedirect(auth, provider);
-    // signInWithPopup(auth, provider)
-    //   .then((result) => {
-    //     const credential = GoogleAuthProvider.credentialFromResult(result);
-    //     const token = credential.accessToken;
-    //     const user = result.user;
-    //     console.log(user, user.uid);
-    //     localStorage.setItem("uid", user.uid);
-    //     localStorage.setItem(
-    //       "userInfo",
-    //       JSON.stringify({
-    //         email: user.email,
-    //         userName: user.displayName,
-    //       })
-    //     );
-    //     DatabaseHandler.registerUserData(
-    //       user.uid,
-    //       user.displayName,
-    //       user.email,
-    //       "null",
-    //       "null"
-    //     );
-    //     authCtx.onLogin();
-    //     navigate("/");
-    //   })
-    //   .catch((error) => {
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    //     const email = error.email;
-    //     const credential = GoogleAuthProvider.credentialFromError(error);
-    //     console.log(errorCode, errorMessage, email, credential);
-    //   });
+    //signInWithRedirect(auth, provider);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+        console.log(user, user.uid);
+        localStorage.setItem("uid", user.uid);
+        localStorage.setItem(
+          "userInfo",
+          JSON.stringify({
+            email: user.email,
+            userName: user.displayName,
+          })
+        );
+        DatabaseHandler.registerUserData(
+          user.uid,
+          user.displayName,
+          user.email,
+          "null",
+          "null"
+        );
+        authCtx.onLogin();
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log(errorCode, errorMessage, email, credential);
+      });
   };
 
   return (
